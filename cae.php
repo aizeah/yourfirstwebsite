b81e4e9769d138f2af52977966322ad52a12b4c9
aizeah <100411831+aizeah@users.noreply.github.com>
2022-02-25 21:16:07 +0800

    Update cae.php

--- cae.php
+++ cae.php
@@ -19,6 +19,15 @@
 <hr>
 <font color=lightpink>
 <h1 align =center>CAE PERFORMANCE TASK</h1>
-
+<font color=beige>
+<img src="cae1.jpg"  border=5 height=400>
+<img src="cae2.jpg"  border=5 height=400>
+<img src="cae3.jpg"  border=5 height=400>
+<img src="cae4.jpg"  border=5 height=400>
+<img src="cae5.jpg"  border=5 height=400>
+<img src="cae6.jpg"  border=5 height=400>
+<img src="cae7.jpg"  border=5 height=400>
+<img src="cae8.jpg"  border=5 height=400>
+<img src="cae9.jpg"  border=5 height=400>
 </body>
 </html>
